976b49526905734cb0b7dcb612d861bc560a2dfd
antonoeschmidt <s163053@student.dtu.dk>
2020-01-05 16:24:15 +0100

    Næsten færdig

--- app/src/main/java/com/example/brintaflevering2/MainActivity.java
+++ app/src/main/java/com/example/brintaflevering2/MainActivity.java
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.AlphaAnimation;
+import android.view.animation.Animation;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -26,6 +28,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         galgeButton.setOnClickListener(this);
         wordListButton = findViewById(R.id.wordListButton);
         wordListButton.setOnClickListener(this);
+
+        Animation anim = new AlphaAnimation(0.0f, 1.0f);
+        anim.setDuration(400);
+        anim.setStartOffset(20);
+        anim.setRepeatMode(Animation.REVERSE);
+        anim.setRepeatCount(Animation.INFINITE);
+        galgeButton.startAnimation(anim);
 
     }
 
